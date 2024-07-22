0fd0e2ad312632d2d09ee71298ac7acfd1b5b9fd
rgu <rgu@WS-5CD0511LN9>
2024-07-22 17:35:31 +0200

    Add project files.

---
+++ Crc_Cal.cpp
@@ -0,0 +1,31 @@
+﻿#include "Crc_Cal.h"
+
+// OBLICZANIE CRC
+uint16_t computeCRC(std::vector<uint8_t> data) {
+    uint16_t crc = INITIAL_VALUE;
+    for (size_t i = 0; i < data.size(); ++i) {
+        crc ^= (data[i] << 7);  // XOR z aktualnym bajtem danych (przesuniętym w lewo)
+
+        for (uint8_t j = 0; j < 8; j++) {   // Przetwarzanie każdego bitu
+            crc <<= 1;
+            if (crc & 0x8000) { // Jeśli najbardziej znaczący bit jest ustawiony
+                crc ^= POLYNOMIAL;  // Przesuwanie w lewo i XOR z wielomianem
+            }
+        }
+    }
+    return crc ^ FINAL_XOR_VALUE;   // XOR na końcu
+}
+
+// STRING BITÓW DO VECTORA BYTEÓW
+std::vector<uint8_t> bitStringToByteArray(std::string& bitString) {
+    std::vector<uint8_t> byteArray;
+    for (int i = (8 - (bitString.size() % 8)); i > 0; i--) {
+        bitString = "0" + bitString;
+    }
+    for (size_t i = 0; i < bitString.size(); i += 8) {
+        std::string byteString = bitString.substr(i, 8);
+        uint8_t byteValue = static_cast<uint8_t>(std::stoi(byteString, nullptr, 2)); // Przekształć ciąg bitów na liczbę całkowitą typu uint8_t
+        byteArray.push_back(byteValue);
+    }
+    return byteArray;
+}
---
+++ Crc_Cal.h
@@ -0,0 +1,20 @@
+﻿#ifndef CRC_CAL_H
+#define CRC_CAL_H
+
+#include <iostream>
+#include <string>
+#include <cstdint>
+#include <vector>
+#include <stdexcept>
+#include <>
+
+// Parametry CRC-15-CAN
+constexpr uint16_t POLYNOMIAL = 0xc599;      // Wielomian CRC-15-CAN (x^15 + x^14 + x^10 + x^8 + x^7 + x^4 + x^3 + 1)
+constexpr uint16_t INITIAL_VALUE = 0x0000;   // Początkowa wartość CRC
+constexpr uint16_t FINAL_XOR_VALUE = 0x000;  // Wartość XOR na końcu obliczeń CRC
+
+// Deklaracje funkcji
+uint16_t computeCRC(std::vector<uint8_t> data);
+std::vector<uint8_t> bitStringToByteArray(std::string& bitString);
+
+#endif // CRC_CAL_H
